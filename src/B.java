ee71dfdd18afc5386be0fa00bd572b04ff362c2b
japanesebonobo <tomi119119@gmail.com>
2020-06-08 21:56:28 +0900

    Update B

--- src/B.java
+++ src/B.java
@@ -2,5 +2,24 @@ import java.util.Scanner;
 
 public class B {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        long K = scanner.nextLong();
+
+        long result;
+
+        if (A >= K) {
+            result = K;
+        }
+        else if (B >= K - A) {
+            result = A;
+        } else {
+            result = -1 * (K - A - B) + A;
+        }
+
+        System.out.println(result);
     }
 }

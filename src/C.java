488175e57fd45e4d0c06ffd93a8e7e73ad33205c
japanesebonobo <tomi119119@gmail.com>
2020-06-10 23:40:40 +0900

    Update C

--- src/C.java
+++ src/C.java
@@ -1,37 +1,49 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class C {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int M = scanner.nextInt();
         int X = scanner.nextInt();
 
-        int [][] A = new int[N][M+1];
+        int [][] A = new int[12][12];
+        int [] C = new int[N];
+
+         int ans = 1000000000;
 
         for (int i = 0; i < N; i++) {
-            int c = scanner.nextInt();
-            A[i][0] = c;
+            C[i] = scanner.nextInt();
             for (int j = 0; j < M; j++) {
-                int a = scanner.nextInt();
-                A[i][j+1] = a;
+                A[i][j] = scanner.nextInt();
             }
         }
 
-        Arrays.sort(A, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0];    //[0] をキーにソート(昇順)
-                //return o2[0] - o1[0];    //[0] をキーにソート(降順)
+        for (int i = 0; i < (1 << N); i++) {
+            int cost = 0;
+            int [] D = new int[M];
+            for (int j = 0; j < N; j++) {
+                if (((i >> j) & 1) != 0) {
+                    cost += C[j];
+                    for (int k = 0; k < M; k++) {
+                        D[k] += A[j][k];
+                    }
+                }
             }
-        });
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M+1; j++) {
-
+            boolean ok = true;
+            for (int k = 0; k < M; k++) {
+                if (D[k] < X) {
+                    ok = false;
+                }
             }
+            if (ok) {
+                ans = Math.min(ans, cost);
+            }
+        }
+        if (ans == 1000000000) {
+            System.out.println(-1);
+        } else  {
+            System.out.println(ans);
         }
     }
 }
